d84cf30a46bb8eb629b30c84cbcfb8e0431c376c
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-26 16:58:04 +0100

    added authentication, needs test with backend

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const LoginForm = props => {
+const LoginForm = ({ submitFormHandler }) => {
   return (
-    <form id="login-form">
+    <form OnSubmit={submitFormHandler} id="login-form">
+
       <label>Email</label>
       <input name="email" type="email" id="email"></input>
-
+      
       <label>Password</label>
       <input name="password" type="password" id="password"></input>
-
       <button id="submit">Submit</button>
     </form>
   );
