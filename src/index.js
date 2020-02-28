3405e0ac92aa1a8a32fc73c03c97813299d9202e
Jaime <jaime.cruz.hostalot@gmail.com>
2020-02-28 14:04:42 +0100

    linking heroku

--- src/index.js
+++ src/index.js
@@ -6,11 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 
 
-axios.defaults.baseURL = "http://localhost:3000/api/v1";
+axios.defaults.baseURL = "https://cooper-api-challenge.herokuapp.com/api/v1";
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
