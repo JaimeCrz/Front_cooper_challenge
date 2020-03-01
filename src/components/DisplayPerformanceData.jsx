70d3eb2948540f6476b466c45e8c1309b0b3501c
Jaime <jaime.cruz.hostalot@gmail.com>
2020-03-01 11:30:05 +0100

    pie pie pie working :tada:

--- src/components/DisplayPerformanceData.jsx
+++ src/components/DisplayPerformanceData.jsx
@@ -25,8 +25,35 @@ class DisplayPerformanceData extends Component {
     })
   }
 
+  
+  //Gets and count every entry entered by the user. We check if entry it's empty too.
+
+  countsCollection(collection, value) {
+    let count = 0;
+    collection.forEach(entry => {
+      count += entry.data.message === value ? 1 : 0;
+    })
+    return count;
+  }
+
+
+  //We are counting how many labels there are, if the indexOf returns -1 JustLabels will return 0 message
+  //as it's empty. using === instead of ==, since == will conver them to common type and make the value be true
+  // while === will return false. 
+
+  onlyLabels(collection) {
+    let justLabels = [];
+    collection.forEach(entry => {
+      if (entry.data.message && justLabels.indexOf(entry.data.message) === -1 ) {
+        justLabels.push(entry.data.message);
+      }
+    })
+    return justLabels;
+  }
+
   render() {
     let dataIndex;
+
 
     if (this.state.performanceData != null) {
       dataIndex = (
@@ -38,14 +65,37 @@ class DisplayPerformanceData extends Component {
       )
     }
 
-    const distances = []
-    const labels = []
+    const distances = [];
+    const labels = [];
+    let dataForPie = {};
 
+    
     if (this.state.performanceData != null) {
       this.state.performanceData.forEach(entry => {
         distances.push(entry.data.distance)
         labels.push(entry.data.message)
       })
+
+      let justLabels = this.onlyLabels(this.state.performanceData);
+      let dataDistance = [];
+
+      justLabels.forEach(label => {
+        dataDistance.push(this.countsCollection(this.state.performanceData, label));
+      })
+
+      dataForPie = {
+        labels: justLabels,
+        datasets: [{
+            data: dataDistance,
+            backgroundColor:[
+              '#33FF79',
+              '#3367FF',
+              '#33BDFF',
+              '#33FFEB',
+              '#33FFC4',
+            ],
+          }],
+      };      
     }
 
     let dataForDiagram = {
@@ -58,23 +108,14 @@ class DisplayPerformanceData extends Component {
       labels: labels
     };
 
-    let dataForPie = {
-      datasets: [
-        {
-          data: distances,
-          label: "Your history"
-        }
-      ],
-      labels: labels
-    };
-
     return (
       <div id="index">
         {dataIndex}
+
         <Line 
         data={dataForDiagram}          
         />
-        
+
         <Pie 
           data= {dataForPie}
         />
